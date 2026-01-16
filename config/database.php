a5307577bac62d4a2b2ff19d836420466e38f7fe
Setya Pujiraharjo - XRPL3 <gaquagame04a@gmail.com>
2026-01-16 20:04:32 +0700

    pindah repo bang

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "mbg_report_db";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if(!$conn){
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi berhasil!";
+}
+
+mysqli_set_charset($conn, "utf8");
+?>
